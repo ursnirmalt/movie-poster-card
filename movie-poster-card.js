9e04692daa23942e2bcfe111a44147792aaf1038
ursnirmalt <ursnirmalt@yahoo.co.in>
2026-02-16 15:04:35 -0500

    Update movie-poster-card.js

--- movie-poster-card.js
+++ movie-poster-card.js
@@ -454,5 +454,5 @@ window.customCards.push({
   name: 'Movie Poster Card',
   description: 'Display movie posters with Apple TV integration',
   preview: true,
-  documentationURL: 'https://github.com/yourusername/movie-poster-card',
+  documentationURL: 'https://github.com/ursnirmalt/movie-poster-card',
 });
